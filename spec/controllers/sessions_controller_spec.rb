dd274b8646ecd3fb334108eb6e4347ece3efe68d
Andreza Medeiros <andreza.cmedeiros@gmail.com>
2014-06-03 23:49:16 -0300

    Test to SessionsController#create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,6 +6,26 @@ describe SessionsController do
   	request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
   end
 
+	describe "#create" do
+		it "should successfully create a user" do
+			expect {
+        post :create, provider: :google_oauth2
+      }.to change{ User.count }.by(1)
+		end
+
+		it "sould successfully create a session" do
+			session[:uid].should be_nil 
+			post :create, provider: :google_oauth2 
+			session[:uid].should_not be_nil
+		end
+
+		it "should returns http success" do
+			visit '/auth/google_oauth2'
+			response.should be_success
+		end
+	end
+
+=begin
   describe "GET 'create'" do
     it "returns http success" do
       get 'create'
@@ -20,5 +40,5 @@ describe SessionsController do
       response.should be_success
     end
   end
-
+=end
 end
